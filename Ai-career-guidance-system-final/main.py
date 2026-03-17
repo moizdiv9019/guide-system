cd7d27d2e8db0b75b06eba9b37076e92e18681a5
Moiz Shedbale <moiz9019455456@gmail.com>
2026-03-17 21:14:10 +0530

    add new code to hide strealit icon

--- Ai-career-guidance-system-final/main.py
+++ Ai-career-guidance-system-final/main.py
@@ -4,9 +4,12 @@ import model
 from myprompts import prompt_1 ,prompt_2
 
 # Hide the "Hosted with Streamlit" footer
+
 hide_st_style = """
             <style>
+            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
